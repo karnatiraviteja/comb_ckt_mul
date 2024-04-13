5ea18d440bc6bb89f58ba0733258971c3e1d4630
karnatiraviteja <157518909+karnatiraviteja@users.noreply.github.com>
2024-04-13 17:25:58 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,19 @@
+// Comb Ckt: 4 bit multiplier
+module mul(
+  input [3:0] a,b,
+  output [7:0] y
+);
+   
+assign y = a * b;
+  
+endmodule
+ 
+ 
+///////////////////////////////////////////
+ 
+interface mul_if;
+  logic [3:0] a;
+  logic [3:0] b;
+  logic [7:0] y;
+  
+endinterface
